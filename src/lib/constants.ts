c5af85ac08a163720dc7031e283006c12cdcd449
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2020-12-21 09:47:08 +0100

    bugfix

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,4 +2,4 @@ import path from 'path';
 
 export const PROJECT_DIR = path.resolve(`${__dirname}/../..`);
 export const DB_PATH = path.resolve(`${PROJECT_DIR}`, 'db.json');
-export const KEYS_DIR = path.resolve(`${PROJECT_DIR}, '.keys/'`)
+export const KEYS_DIR = path.resolve(`${PROJECT_DIR}`, '.keys/')
